93662a062668c05c1071649e67d6b6d926292941
Alex Black <wa7@sa34.cx>
2021-12-23 19:30:14 +0300

    First test

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,12 @@
-use sqlx::postgres::PgPoolOptions;
+mod postgres;
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
-    // Create a connection pool
-    //  for MySQL, use MySqlPoolOptions::new()
-    //  for SQLite, use SqlitePoolOptions::new()
-    //  etc.
-    // let pool = PgPoolOptions::new()
-    //     .max_connections(5)
-    //     .connect("postgres://postgres:password@localhost/test").await?;
+    dotenv::dotenv().ok();
 
-    // // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL)
-    // let row: (i64,) = sqlx::query_as("SELECT $1")
-    //     .bind(150_i64)
-    //     .fetch_one(&pool).await?;
+    let pool = postgres::connect().await.unwrap();
 
-    // assert_eq!(row.0, 150);
-
-    println!("Hello!");
+    
 
     Ok(())
 }
