7cc0764f47a73e16328fbbb0dce170e9f19d8122
Alex Black <wa7@sa34.cx>
2021-12-23 22:47:53 +0300

    Add axum

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,61 @@
+use axum::{
+    extract::Extension, http::StatusCode, response::IntoResponse, routing::get, AddExtensionLayer,
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use sqlx::postgres::PgPool;
+    
 mod postgres;
 
+#[derive(Serialize)]
+struct Test {
+    id: u64,
+    text: String,
+}
+
 #[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
+async fn main() {
     dotenv::dotenv().ok();
 
     let pool = postgres::connect().await.unwrap();
 
-    let sql = "INSERT INTO wa7_test_sqlx (text) VALUES ('test')".to_string();
+    let app = Router::new()
+        .route("/", get(test_handler))
+        .layer(AddExtensionLayer::new(pool));;
 
-    sqlx::query(&sql).execute(&pool).await?;
+    let host = dotenv::var("HOST").expect("Couldn't read HOST from .env file!");
 
-    Ok(())
+    println!("Listening on: {}", &host);
+
+    axum::Server::bind(&host.parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
+
+async fn test_handler(Extension(pool): Extension<PgPool>,) -> impl IntoResponse {
+    // insert your application logic here
+    let test = Test {
+        id: 2,
+        text: "abc".to_string(),
+    };
+
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::OK, Json(test))
+}
+
+// mod postgres;
+
+// #[tokio::main]
+// async fn main() -> Result<(), sqlx::Error> {
+//     dotenv::dotenv().ok();
+
+//     let pool = postgres::connect().await.unwrap();
+
+//     let sql = "INSERT INTO wa7_test_sqlx (text) VALUES ('test')".to_string();
+
+//     sqlx::query(&sql).execute(&pool).await?;
+
+//     Ok(())
+// }
--- src/postgres.rs
+++ src/postgres.rs
@@ -1,17 +1,21 @@
-use sqlx::{Error, postgres::{PgConnectOptions, PgPool}};
+//! Provides the connection pool for asynchronous PostgreSQL connections.
+use sqlx::{
+    postgres::{PgConnectOptions, PgPool},
+    Error,
+};
 
+/// Creates a new connection pool
 pub async fn connect() -> Result<PgPool, Error> {
-    // PgConnectOptions::new().connect().await?
-
-    // let options = PgConnectOptions::new()
-    // .host("secret-host")
-    // .port(2525)
-    // .username("secret-user")
-    // .password("secret-password")
-    // .ssl_mode(PgSslMode::Require);
-
+    // By default, this reads the following environment variables
+    // and sets their equivalent options.
+    // See docs: https://docs.rs/sqlx/latest/sqlx/postgres/struct.PgConnectOptions.html#impl
     let options = PgConnectOptions::new();
 
+    // Creates a new connection pool with a default pool configuration
+    // and the given connection options; and, immediately establishes one connection.
+    //
+    // max_connections: 10
+    // min_connections: 0
+    // connect_timeout: 30sec
     PgPool::connect_with(options).await
-
 }
